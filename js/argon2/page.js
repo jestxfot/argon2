bffd6391a0a4631ec756d73e43dc258f3f620e9f
GitHub <jestx@jestx>
2024-02-11 20:50:29 +0000

    11 февр. 2024 г., 23:50

--- js/argon2/page.js
+++ js/argon2/page.js
@@ -122,72 +122,101 @@
         }
     }
 
-    function argon2Hash(params) {
-        const mCost = params.mem || 1024;
-        return loadModule(mCost)
-            .then(Module => {
-                const tCost = params.time || 1;
-                const parallelism = params.parallelism || 1;
-                const pwdEncoded = encodeUtf8(params.pass);
-                const pwd = allocateArray(Module, pwdEncoded);
-                const pwdlen = pwdEncoded.length;
-                const saltEncoded = encodeUtf8(params.salt);
-                const salt = allocateArray(Module, saltEncoded);
-                const saltlen = saltEncoded.length;
-                const hash = Module.allocate(new Array(params.hashLen || 24), 'i8', Module.ALLOC_NORMAL);
-                const hashlen = params.hashLen || 24;
-                const encoded = Module.allocate(new Array(512), 'i8', Module.ALLOC_NORMAL);
-                const encodedlen = 512;
-                const argon2Type = params.type || ArgonType.Argon2d;
-                const version = 0x66;
-                let err;
-                let res;
-                try {
-                    res = Module._argon2_hash(tCost, mCost, parallelism, pwd, pwdlen, salt, saltlen, hash, hashlen, encoded, encodedlen, argon2Type, version);
-                } catch (e) {
-                    err = e;
+// Функция для хеширования пароля с использованием алгоритма Argon2
+function argon2Hash(params) {
+    // Устанавливаем значение по умолчанию для объема памяти, если не указано
+    const mCost = params.mem || 1024;
+
+    // Загружаем модуль Argon2
+    return loadModule(mCost)
+        .then(Module => {
+            // Устанавливаем значение по умолчанию для времени выполнения, если не указано
+            const tCost = params.time || 1;
+            // Устанавливаем значение по умолчанию для параллелизма, если не указано
+            const parallelism = params.parallelism || 1;
+
+            // Кодируем пароль в UTF-8
+            const pwdEncoded = encodeUtf8(params.pass);
+            // Выделяем память для хранения пароля в модуле
+            const pwd = allocateArray(Module, pwdEncoded);
+            const pwdlen = pwdEncoded.length;
+
+            // Кодируем соль в UTF-8
+            const saltEncoded = encodeUtf8(params.salt);
+            // Выделяем память для хранения соли в модуле
+            const salt = allocateArray(Module, saltEncoded);
+            const saltlen = saltEncoded.length;
+
+            // Выделяем память для хранения хэша в модуле
+            const hash = Module.allocate(new Array(params.hashLen || 24), 'i8', Module.ALLOC_NORMAL);
+            const hashlen = params.hashLen || 24;
+
+            // Выделяем память для хранения закодированных данных в модуле
+            const encoded = Module.allocate(new Array(512), 'i8', Module.ALLOC_NORMAL);
+            const encodedlen = 512;
+
+            // Устанавливаем тип алгоритма Argon2, если не указано
+            const argon2Type = params.type || ArgonType.Argon2d;
+            const version = 0x66;
+
+            let err;
+            let res;
+
+            // Вызываем функцию хеширования пароля
+            try {
+                res = Module._argon2_hash(tCost, mCost, parallelism, pwd, pwdlen, salt, saltlen, hash, hashlen, encoded, encodedlen, argon2Type, version);
+            } catch (e) {
+                err = e;
+            }
+
+            let result;
+            if (res === 0 && !err) {
+                // Пароль успешно закодирован
+                let hashStr = '';
+                const hashArr = new Uint8Array(hashlen);
+                for (let i = 0; i < hashlen; i++) {
+                    const byte = Module.HEAP8[hash + i];
+                    hashArr[i] = byte;
+                    hashStr += ('0' + (0xff & byte)
+                            .toString(16))
+                        .slice(-2);
                 }
-                let result;
-                if (res === 0 && !err) {
-                    let hashStr = '';
-                    const hashArr = new Uint8Array(hashlen);
-                    for (let i = 0; i < hashlen; i++) {
-                        const byte = Module.HEAP8[hash + i];
-                        hashArr[i] = byte;
-                        hashStr += ('0' + (0xff & byte)
-                                .toString(16))
-                            .slice(-2);
+                const encodedStr = Module.UTF8ToString(encoded);
+                result = {
+                    hash: hashArr,
+                    hashHex: hashStr,
+                    encoded: encodedStr
+                };
+            } else {
+                // Возникла ошибка при хешировании пароля
+                try {
+                    if (!err) {
+                        err = Module.UTF8ToString(Module._argon2_error_message(res));
                     }
-                    const encodedStr = Module.UTF8ToString(encoded);
-                    result = {
-                        hash: hashArr,
-                        hashHex: hashStr,
-                        encoded: encodedStr
-                    };
-                } else {
-                    try {
-                        if (!err) {
-                            err = Module.UTF8ToString(Module._argon2_error_message(res));
-                        }
-                    } catch (e) {}
-                    result = {
-                        message: err,
-                        code: res
-                    };
-                }
-                try {
-                    Module._free(pwd);
-                    Module._free(salt);
-                    Module._free(hash);
-                    Module._free(encoded);
                 } catch (e) {}
-                if (err) {
-                    throw result;
-                } else {
-                    return result;
-                }
-            });
-    }
+                result = {
+                    message: err,
+                    code: res
+                };
+            }
+
+            // Освобождаем выделенную память
+            try {
+                Module._free(pwd);
+                Module._free(salt);
+                Module._free(hash);
+                Module._free(encoded);
+            } catch (e) {}
+
+            // Если возникла ошибка, выбрасываем исключение, иначе возвращаем результат
+            if (err) {
+                throw result;
+            } else {
+                return result;
+            }
+        });
+}
+
 
     function argon2Verify(params) {
         return loadModule()
